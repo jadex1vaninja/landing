8369a97b1af8c5afe15a32761729259d7c8d47b7
Ivaninja <wanyaka1@gmail.com>
2021-04-18 16:18:01 +0300

    prevent default for mail

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const submit = document.getElementById("submit");
 
-submit.addEventListener("click", async () => {
+submit.addEventListener("click", async (evt) => {
+  evt.preventDefault()
   const email = document.querySelector("#email").value;
   const name = document.querySelector("#name").value ?? '';
   const surname = document.querySelector("#surname").value ?? '';
