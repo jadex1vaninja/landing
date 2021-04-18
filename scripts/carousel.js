e5f7a024d31b18adbd0c3a643cff09f4835ba4fa
jadex1vaninja <ivan.lazarev@jadexconsulting.com>
2021-04-18 17:54:58 +0300

    hide carousel

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,21 +1,21 @@
 $(document).ready(function () {
-  $('.carousel__carousel-wrapper').slick({
-    infinite: true,
-    speed: 500,
-    cssEase: 'linear',
-    useTransform: false,
-    arrows: true,
-    responsive: [
-      {
-        breakpoint: 800,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-    ],
-  });
+  // $('.carousel__carousel-wrapper').slick({
+  //   infinite: true,
+  //   speed: 500,
+  //   cssEase: 'linear',
+  //   useTransform: false,
+  //   arrows: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 800,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //         arrows: false,
+  //       },
+  //     },
+  //   ],
+  // });
 
   $('.card__card-wrapper').slick({
     speed: 500,
