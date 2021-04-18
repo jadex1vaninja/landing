be7078981b1d9e043613026005714add881c33cd
Ivaninja <wanyaka1@gmail.com>
2021-04-18 14:14:26 +0300

    remove keys

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// const key = "db7cc126-3319-4252-afc7-8db88b40c8f8";
-const SecureToken = "d4c174b5-3612-4325-a54d-bf60d116aa6f"; //465
-const SecureToken2 = "9a07a771-5a21-4f5a-943f-99ac82728e9d"; //587
-
 const submit = document.getElementById("submit");
 
 submit.addEventListener("click", async () => {
@@ -10,7 +6,7 @@ submit.addEventListener("click", async () => {
   const surname = document.querySelector("#surname").value ?? '';
   if(!email || !name || !surname) return;
 
-  
+
   const res = await fetch(
     `https://mailapi.vercel.app/api/send-mail?email=${email}&name=${name}&surname=${surname}`
   );
